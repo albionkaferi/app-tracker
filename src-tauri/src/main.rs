467c4d8b52c205af7c10576e302e45229dbe5db4
albionkaferi <albionkaferi02@gmail.com>
2023-07-24 13:09:03 -0400

    added new thread for tracking loop

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod track;
+use std::thread;
+use track::track_processes;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -10,6 +12,10 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    thread::spawn(move || {
+        track_processes();
+    });
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
