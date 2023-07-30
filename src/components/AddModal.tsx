54e00a1e635407ff2fcaca073884a4006a7256aa
Tyler <tylerjdunham1@gmail.com>
2023-07-30 17:07:20 -0400

    Add upload

--- src/components/AddModal.tsx
+++ src/components/AddModal.tsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
-import { Button, Modal, Input} from 'antd';
+import { Button, Modal, Input, Upload} from 'antd';
 import TimeInput from "./TimeInput";
 import { error, success } from "../helpers";
+import { UploadOutlined } from '@ant-design/icons';
+import { RcFile } from 'antd/lib/upload';
 
 function AddModal({add_app}: {add_app: (name: string, allowed: number) => Promise<string>}) {
     const [open, setOpen] = useState(false);  
     const [name, setName] = useState("");
     const [allowed, setAllowed] = useState(0);
+
+    const preventUpload = (file: RcFile) => {
+      setName(`${file.name.slice(0, -4)}`)
+      return false;
+    }
     
     async function onClickHandler() {
-      const result: string = await add_app(name, allowed);
+      const result: string = await add_app(name+".exe", allowed);
       if (result.startsWith('Error')) {
         error(result)
       }
@@ -39,13 +46,14 @@ function AddModal({add_app}: {add_app: (name: string, allowed: number) => Promis
         onCancel={closeModal}
         footer={[]}
       >
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ display: 'flex', alignItems: 'center', paddingBottom: "3px" }}>
           <Input
-            style={{ width: '40%' }}
-            id="name-input"
+            style={{width: "32%"}}
+            id="app-input"
             autoComplete="off"
             onChange={(e) => setName(e.currentTarget.value)}
             placeholder="App name..."
+            value={name}
             suffix=".exe"
           />
           <TimeInput setAllowed={setAllowed} />
@@ -56,6 +64,17 @@ function AddModal({add_app}: {add_app: (name: string, allowed: number) => Promis
           >
             Add
           </Button>
+        </div>
+        <div>
+          <Upload
+            listType="text"
+            accept=".exe"
+            maxCount={1}
+            beforeUpload={preventUpload}
+            showUploadList={false}
+          >
+            <Button icon={<UploadOutlined />}>Choose file</Button>
+          </Upload>
         </div>
       </Modal>
       </>
